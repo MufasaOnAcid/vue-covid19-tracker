8cc4d6aec01f2deedb7a78d0000d2b315c6632c7
Mustafa Jeja <mustafajeja@gmail.com>
2021-04-25 02:56:36 +0200

    Update Color scheme

--- src/components/DataBoxes.vue
+++ src/components/DataBoxes.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="grid md:grid-cols-2 gap-4">
     <!-- Box 1 -->
-    <div class="shadow-md bg-blue-100 p-10 text-center rounded">
-      <h3 class="text-3xl text-blue-900 font-bold mb-4">
+    <div class="shadow-md bg-purple-100 p-10 text-center rounded">
+      <h3 class="text-3xl text-purple-900 font-bold mb-4">
         Cases
       </h3>
       <div class="text-2xl mb-4">
@@ -16,8 +16,8 @@
     </div>
 
     <!-- Box 2 -->
-    <div class="shadow-md bg-red-100 p-10 text-center rounded">
-      <h3 class="text-3xl text-blue-900 font-bold mb-4">
+    <div class="shadow-md bg-purple-200 p-10 text-center rounded">
+      <h3 class="text-3xl text-purple-900 font-bold mb-4">
         Deaths
       </h3>
       <div class="text-2xl mb-4">
